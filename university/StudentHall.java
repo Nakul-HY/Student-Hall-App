4f6f7b51e7a233fb840b79945ee6e83ca7eb4ec1
Your Name <you@example.com>
2024-10-31 04:14:05 +0000

    sample

---
+++ university/StudentHall.java
@@ -0,0 +1,14 @@
+package university;
+
+public class StudentHall {
+    private Student students[];
+
+    public static void main(String args[])
+    {
+        System.out.println("Student Hall Testingg");
+    }
+    public boolean onPremise()
+    {
+        return bool;
+    }
+}
